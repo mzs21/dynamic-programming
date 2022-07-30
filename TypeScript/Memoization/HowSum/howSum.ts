8553f26b4a6fa7b2f9a164cbec115b652c8f6b3b
mzs21 <snigdho2111@gmail.com>
2022-07-30 20:38:18 +0600

    Typo fixed

--- TypeScript/Memoization/HowSum/howSum.ts
+++ TypeScript/Memoization/HowSum/howSum.ts
@@ -1,4 +1,4 @@
-// Write a function 'canSum(targetSum, numbers)' that takes in a targetSum and an array of numbers as arguments.
+// Write a function 'howSum(targetSum, numbers)' that takes in a targetSum and an array of numbers as arguments.
 
 // The function should return an array containing any combination of elements that add up to exactly the targetSum. If there is no combination that adds up to the targetSum, then return null
 
